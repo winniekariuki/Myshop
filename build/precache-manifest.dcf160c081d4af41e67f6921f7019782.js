88953202e1af94b2f74523eb6e8dad70147efcf4
winniekariuki <winniekariuki07@gmail.com>
2019-12-09 01:43:28 -0800

    deployement of app

---
+++ build/precache-manifest.dcf160c081d4af41e67f6921f7019782.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b57bec90d51af0a054cc02c8b18480",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f61cc4e44f60ad80181a",
+    "url": "/static/css/2.28dcdecb.chunk.css"
+  },
+  {
+    "revision": "08351dca200fbcd53144",
+    "url": "/static/css/main.12b378f1.chunk.css"
+  },
+  {
+    "revision": "f61cc4e44f60ad80181a",
+    "url": "/static/js/2.0e53acb2.chunk.js"
+  },
+  {
+    "revision": "08351dca200fbcd53144",
+    "url": "/static/js/main.1be1cb03.chunk.js"
+  },
+  {
+    "revision": "fe06e1df1e92d292d3c6",
+    "url": "/static/js/runtime-main.26ccbf28.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
