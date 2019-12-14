8caef1d43443e3695672c3e700ea0f6662c1c0f5
winniekariuki <winniekariuki07@gmail.com>
2019-12-14 00:41:34 -0800

    fix footer

--- src/Components/CustomerCareCard/Card.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import CardCustomer from "./index";
-import { shallow } from "enzyme";
-
-it("renders without crashing", () => {
-  const component = shallow(<CardCustomer />);
-  expect(component).toMatchSnapshot();
-});
